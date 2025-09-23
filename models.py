6332a9f156fe647b6c7981d4d8a22c46969e9ab0
LenchMay <lenchikmay@bk.ru>
2025-09-23 17:40:20 +0300

    HW2 - добавление моделей поста и комментария

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, server_default=db.func.now())
+    comments = db.relationship('Comment', backref='post', lazy=True)
+
+    def __repr__(self):
+        return '<Post {self.title}>'
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Comment {self.text[:20]}>'
