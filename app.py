503018f3b9d5d4c6b5bdc9cd47dce8825d9c8d57
LenchMay <lenchikmay@bk.ru>
2025-09-24 11:41:49 +0300

    HW2 - добавлена html оболочка поста и комментария, модифыицирован app.py для работы с БД

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
+from models import db, Post, Comment
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return '<h1>Welcome to Blog Post</h1>'
+
+@app.route('/posts')
+def all_posts():
+    posts = Post.query.all()
+    return render_template('posts.html', posts=posts)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/post/<int:post_id>/comments')
+def show_comments(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('comments.html', post=post)
 
 @app.route('/contacts')
 def contacts():
-    cnt = '1. Elena<br>' \
-    '2. Nikolay'
+    cnt =   '1. Elena<br>' \
+            '2. Nikolay'
     return cnt
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8088)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
