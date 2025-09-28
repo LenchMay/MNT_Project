022c4d74977cc95490f12fac32aa18cf8aa42473
LenchMay <lenchikmay@bk.ru>
2025-09-28 17:06:53 +0300

    HW4 В конфиг добавлены настройки JWT

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 import os
+from datetime import timedelta
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # JWT настройки
+    JWT_SECRET_KEY = 'super-secret'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+    JWT_TOKEN_LOCATION = ['headers']
+    JWT_HEADER_NAME = 'Authorization'
+    JWT_HEADER_TYPE = 'Bearer'
+    PROPAGATE_EXCEPTIONS = True
